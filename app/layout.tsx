0027f4d58949b7d5b999b744425db0eaeba0aae9
predatorgate <predatorgate@proton.me>
2025-12-03 13:58:29 +0200

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono, Playfair_Display } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
+import Script from "next/script";
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
@@ -26,6 +27,22 @@ export default function RootLayout({
   return (
     <html lang="el" className={_playfair.variable}>
       <body className={`font-sans antialiased`}>
+        {/* Google Analytics tag */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-XF5R1NQ8BH`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+
         {children}
         <Analytics />
       </body>
