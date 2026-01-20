296f57dfaedbde15eb3c529f9633b3a1036f635f
predatorgate <predatorgate@proton.me>
2026-01-20 10:47:41 +0200

    Fix google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono, Playfair_Display } from "next/font/google"
 import "./globals.css"
-import Script from "next/script";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
@@ -26,24 +26,9 @@ export default function RootLayout({
   return (
     <html lang="el" className={_playfair.variable}>
       <body className={`font-sans antialiased`}>
-        {/* Google Analytics tag */}
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-XF5R1NQ8BH`}
-          strategy="afterInteractive"
-        />
-        <Script id="ga-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
-
         {children}
       </body>
+      <GoogleAnalytics gaId="G-XF5R1NQ8BH" />
     </html>
   )
 }
